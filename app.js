5ba801159288383b9ef6b2dc249fcf3bb3cdc3b8
Kitchu0401 <kitchu@naver.com>
2017-05-29 00:58:39 +0900

    설정파일로부터 구동 포트 읽어들이도록 변경

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 let express = require('express')
 let mecab = require('mecab-ya')
+let config = require('./utils/staticConfig')
 
+// middlewares
 let morgan = require('morgan')
 let bodyParser = require('body-parser')
 
+// routers
 let nlp = require('./routes/nlp')
 let keyboard = require('./routes/kakao-keyboard')
 let message = require('./routes/kakao-message')
 
 const app = express()
-const port = 5000
+const port = config.port || 3000
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
